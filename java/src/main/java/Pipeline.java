ea85828d460401c1d29ad4fd801c7625505db053
dmitry.kandalov <dmitry.kandalov@gmail.com>
2020-06-25 10:00:17 +0100

    refactoring

--- java/src/main/java/Pipeline.java
+++ java/src/main/java/Pipeline.java
@@ -49,15 +49,13 @@ public class Pipeline {
     }
 
     private boolean deploy(Project project, boolean testsPassed) {
-        if (testsPassed) {
-            if ("success".equals(project.deploy())) {
-                log.info("Deployment successful");
-                return true;
-            } else {
-                log.error("Deployment failed");
-                return false;
-            }
+        if (!testsPassed) return false;
+
+        if ("success".equals(project.deploy())) {
+            log.info("Deployment successful");
+            return true;
         } else {
+            log.error("Deployment failed");
             return false;
         }
     }
